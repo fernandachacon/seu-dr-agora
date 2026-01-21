33b261405e5f904aae0cfcc56052f7783779f83e
Mariana Dantas <marianamesquita16@gmail.com>
2026-01-20 21:15:35 -0300

    Form como página para cadastro e outras pequenas mudancas no scss

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,16 +1,66 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Header } from '../header/header';
-import { BtnPrimaryComponent } from '../btn-primary/btn-primary';
+import { Footer } from '../footer/footer';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-form',
+  standalone: true,
   imports: [
+    CommonModule,
+    ReactiveFormsModule,
     Header,
-    BtnPrimaryComponent
+    Footer,
+    NgOptimizedImage
   ],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
 export class Form {
+  etapa = 1;
 
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+});
+}
+
+  proximaEtapa() {
+  if (this.etapa === 1) {
+    const camposEtapa1 = ['nome', 'email'];
+
+    camposEtapa1.forEach(campo => {
+      this.form.get(campo)?.markAsTouched();
+    });
+
+    if (
+      this.form.get('nome')?.invalid ||
+      this.form.get('email')?.invalid
+    ) {
+      return;
+    }
+  }
+
+  this.etapa++;
+}
+
+  voltarEtapa() {
+    this.etapa--;
+  }
+
+  finalizarCadastro() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('Dados do cadastro:', this.form.value);
+  }
 }
